d43672d766bf202adef273f08d67996a16449542
sahal-shalu <sahalshalu5426@gmail.com>
2023-06-19 02:07:35 +0530

    BinerySearchTreeInsertion

---
+++ Tree/BinerySerarchTree.js
@@ -0,0 +1,51 @@
+class Node{
+    constructor(value){
+        this.value=value
+        this.left=null
+        this.right=null
+
+    }
+}
+
+class BinerySearchTree{
+    constructor() {
+        this.root=null
+    }
+    isEmpty(){
+        return this.root===null
+    }
+
+    insert(value){
+        const newNode=new Node(value)
+    if(this.isEmpty()){
+        this.root=newNode
+    }else{
+        this.insertNode(this.root,newNode)
+    }
+    }
+
+    insertNode(root,newNode){
+        if(newNode.value<root.value){
+            if(root.left===null){
+                root.left=newNode
+            }else{
+                this.insertNode(root.left,newNode )
+            }
+            }else{
+                if(root.right===null){
+                    root.right=newNode
+                }else{
+                    this.insertNode(root.right,newNode)
+                }
+        }
+    }
+}
+
+
+const bst=new BinerySearchTree()
+
+
+
+bst.insert(10)
+bst.insert(5)
+bst.insert(15)
