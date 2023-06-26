52866366a1c9d071ffd48624f4bb8f4b534ca3e8
sahal-shalu <sahalshalu5426@gmail.com>
2023-06-26 12:28:25 +0530

    Graph completed

--- Graph/graph.js
+++ Graph/graph.js
@@ -19,17 +19,90 @@ class Graph{
         this.List[vertex2].add(vertex1)
     }
 
-    
+    hasEdges(vertex1,vertex2){
+        return (this.List[vertex1].has(vertex2) && this.List[vertex2].has(vertex1))
+    }
 
+
+    display(){
+        for(let vertex in this.List){
+            console.log(vertex+'->'+[...this.List[vertex]])
+        }
+    }
+
+    removeEdges(vertex1,vertex2){
+        this.List[vertex1].delete(vertex2)
+        this.List[vertex2].delete(vertex1)
+    }
+
+    removeVertex(vertex){
+        if(!this.List[vertex]){
+            return null
+        }else{
+            for(let removeVertex in this.List){
+                this.removeEdges(vertex,removeVertex)
+            }
+            delete this.List[vertex]
+        }
+    }
+
+    dfs(start){
+       let visited=new Set()
+       let result=[]
+       const dfsHelper=(vertex)=>{
+       visited.add(vertex)
+       result.push(vertex)
+       this.List[vertex].forEach(neighbor=>{
+        if(!visited.has(neighbor)){
+            dfsHelper(neighbor)
+        }
+       })
+    }
+    dfsHelper(start)
+    return result
+}
+
+
+bfs(start){
+    const queue=[start]
+    let visited=new Set(start)
+    let result=[]
+    while(queue.length){
+        let cur=queue.shift()
+        result.push(cur)
+        this.List[cur].forEach((neighbor)=>{
+            if(!visited.has(neighbor)){
+                queue.push(neighbor)
+                visited.add(neighbor)
+            }
+        })
+    }
+    return result;
+  }
 }
 
 const graph=new Graph()
-graph.addVertex('A')
-graph.addVertex('B')
-graph.addVertex('C')
-graph.addVertex('D')
+graph.addVertex("A")
+graph.addVertex("B")
+graph.addVertex("C")
+graph.addVertex("D")
+graph.addVertex("E")
+graph.addVertex("F")
 
 
-graph.addEdges('A','B')
-graph.addEdges('B','c')
-graph.addEdges('A','D')
+
+
+graph.addEdges("A","B")
+graph.addEdges("A","C")
+graph.addEdges("B","D")
+graph.addEdges("C","E")
+graph.addEdges("D","E")
+graph.addEdges("D","F")
+graph.addEdges("A","E")
+
+graph.display()
+console.log('.......')
+console.log(graph)
+console.log(graph.dfs('A'));
+console.log(graph.bfs("A"))
+
